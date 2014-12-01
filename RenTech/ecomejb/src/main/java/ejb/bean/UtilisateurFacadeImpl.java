7274c0101b976d3f46ca68db4172066aa4a4ba1b
Augustin <nexucis@gmail.com>
2014-12-01 23:17:16 +0100

    almost almost ....

--- RenTech/ecomejb/src/main/java/ejb/bean/UtilisateurFacadeImpl.java
+++ RenTech/ecomejb/src/main/java/ejb/bean/UtilisateurFacadeImpl.java
@@ -2,6 +2,7 @@ package ejb.bean;
 
 import ejb.entity.Utilisateur;
 
+import javax.annotation.sql.DataSourceDefinition;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceUnit;
@@ -10,6 +11,18 @@ import java.io.Serializable;
 /**
  * Created by augustin on 25/11/14.
  */
+
+@DataSourceDefinition(name="derby",
+        minPoolSize = 0,
+        initialPoolSize = 0,
+        className = "org.apache.derby.jdbc.ClientDataSource",
+        portNumber = 1527,
+        serverName = "localhost",
+        user = "ecom",
+        password = "ecom",
+        databaseName = "ecomBdd",
+        properties = {"connectionAttibutes=;create=true"}
+)
 
 @Stateless
 public class UtilisateurFacadeImpl implements UtilisateurFacadeRemote, Serializable{
