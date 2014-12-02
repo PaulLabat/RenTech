9e789660c10747110b0417b0fa05af2f7e29c2d6
Augustin <nexucis@gmail.com>
2014-12-02 11:50:13 +0100

    correction du script sh

--- RenTech/deploy.sh
+++ RenTech/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-message=`hostname`;
-echo $message
-echo "http://$message:8080/ecom/"
+host=`hostname`;
 asadmin undeploy ecom
 asadmin stop-database
 asadmin stop-domain
@@ -10,4 +8,4 @@ asadmin start-domain
 asadmin start-database
 mvn clean install
 asadmin redeploy --name ecomear --contextroot "ecom" ecomear/target/ecomear-0.1.0.ear
-firefox "http://$message:8080/ecom/"
+firefox "http://$host:8080/ecom/"
