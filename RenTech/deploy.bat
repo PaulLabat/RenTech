894802828e56ccd3f00d74923998efed338c9f14
Augustin <nexucis@gmail.com>
2014-12-02 13:47:14 +0100

    ajout du script windows

---
+++ RenTech/deploy.bat
@@ -0,0 +1,14 @@
+(
+host=`hostname`;
+asadmin undeploy ecom
+asadmin stop-database
+asadmin stop-domain
+echo " ########################################"
+echo " ####### Restarting glassfish ... #######"
+echo " ########################################\n"
+asadmin start-domain
+asadmin start-database
+mvn clean install
+asadmin redeploy --name ecomear --contextroot "ecom" ecomear/target/ecomear-0.1.0.ear
+xdg-open "http://$host:8080/ecom/"
+)
