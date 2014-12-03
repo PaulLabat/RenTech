b75676b28c6a3937d88987232fa98cb663ff97c1
Augustin <nexucis@gmail.com>
2014-12-03 21:43:52 +0100

    suppression de warning

--- RenTech/deploy.sh
+++ RenTech/deploy.sh
@@ -9,23 +9,23 @@ host=`hostname`;
 DeployementWithXML(){
 #deploiement du site avec settings.xml
 echo "deploiement du site avec settings.xml"
-asadmin --user $admin undeploy ecomear
-asadmin --user $admin stop-database
-asadmin --user $admin stop-domain
+asadmin --user $(admin) undeploy ecomear
+asadmin --user $(admin) stop-database
+asadmin --user $(admin) stop-domain
 echo "\n ########################################"
 echo " ####### Restarting glassfish ... #######"
 echo " ########################################\n"
-asadmin --user $admin start-domain
-asadmin --user $admin start-database
+asadmin --user $(admin) start-domain
+asadmin --user $(admin) start-database
 mvn clean install
-asadmin --user $admin redeploy --name ecomear --contextroot "ecom" ecomear/target/ecomear-0.1.0.ear
+asadmin --user $(admin) redeploy --name ecomear --contextroot "ecom" ecomear/target/ecomear-0.1.0.ear
 xdg-open "http://$host:8080/ecom/"
 }
 
 DeployementWithoutXML(){
 #deploiement du site sans settings.xml
 echo "deploiement du site sans settings.xml"
-asadmin $admin undeploy ecom
+asadmin undeploy ecom
 asadmin stop-database
 asadmin stop-domain
 echo "\n ########################################"
@@ -38,11 +38,11 @@ asadmin redeploy --name ecomear --contextroot "ecom" ecomear/target/ecomear-0.1.
 xdg-open "http://$host:8080/ecom/"
 }
 
-if [ -e $settingsXML ]
+if [ -e $(settingsXML) ]
 then
     echo "##### fichier settings.xml trouvé #####"
-    password=`sed -n -r 's%<local.glassfish.adminPassword>(.*)<\/local.glassfish.adminPassword>%\1%p' $settingsXML`
-    admin=`sed -n -r 's%<local.glassfish.user>(.*)<\/local.glassfish.user>%\1%p' $settingsXML`
+    password=`sed -n -r 's%<local.glassfish.adminPassword>(.*)<\/local.glassfish.adminPassword>%\1%p' $(settingsXML)`
+    admin=`sed -n -r 's%<local.glassfish.user>(.*)<\/local.glassfish.user>%\1%p' $(settingsXML)`
     DeployementWithXML
 else
     echo "##### le fichier settings.xml n'a pas été trouvé #####"
