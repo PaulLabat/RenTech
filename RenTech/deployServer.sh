c5c9d11026fa3d63ab5e930679fd14998f4cc87f
Augustin <nexucis@gmail.com>
2014-12-15 22:23:36 +0100

    mise en place d'un script de déploiement sur un vrai serveur

---
+++ RenTech/deployServer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+asadmin='/home/augustin/glassfish4/glassfish/bin/./asadmin';
+ij='/home/augustin/glassfish4/javadb/bin/./ij';
+DeleteBDD(){
+$ij deleteTable.sql;
+}
+CreateAdminAccess(){
+$ij createAdminUser.sql;
+}
+$asadmin stop-database
+$asadmin stop-domain
+echo "\n ########################################"
+echo " ####### Restarting glassfish ... #######"
+echo " ########################################\n"
+$asadmin start-domain
+$asadmin undeploy ecomear
+$asadmin start-database
+DeleteBDD
+$asadmin deploy --name ecomear --contextroot "ecom" target/ecomear-0.1.0.ear
+$asadmin get-client-stubs --appname ecomear target/
+CreateAdminAccess
