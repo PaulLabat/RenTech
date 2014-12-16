6b4e5a790b8050cff832298ac53f19ae40167f1d
Paul <paul.mariage@live.fr>
2014-12-16 10:34:57 +0100

    modifyCommande ajouté

--- RenTech/ecomweb/src/main/java/servlets/Services.java
+++ RenTech/ecomweb/src/main/java/servlets/Services.java
@@ -99,6 +99,7 @@ public class Services {
         if (fonct.compareTo("connectUser")==0) ServiceUser.onConnectUser(ufi,session,jsonObject);
         else if (fonct.compareTo("createUser")==0) ServiceUser.onCreateUser(ufi,session,jsonObject);
         else if (fonct.compareTo("pushCommande")==0) ServiceCommande.onPushCommande(cfi,session,jsonObject);
+        else if (fonct.compareTo("modifyCommande")==0) ServiceCommande.onModifyCommande(cfi,session,jsonObject);
         else if (fonct.compareTo("changeInfos")==0) ServiceUser.onChangeInfos(ufi,session,jsonObject);
         else if (fonct.compareTo("deleteAccount")==0) ServiceUser.onDeleteAccount(ufi,session,jsonObject);
         else if (fonct.compareTo("getUsers")==0) ServiceUser.onGetUsers(ufi,session,jsonObject);
