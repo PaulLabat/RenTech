4c14143bdc59bf02b8b27e80f6ec05020639de50
Augustin <nexucis@gmail.com>
2014-12-16 23:07:18 +0100

    modification du script de deploiement sur le serveur

--- RenTech/server/deployServer.sh
+++ RenTech/server/deployServer.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 asadmin='/home/augustin/glassfish4/glassfish/bin/./asadmin';
 ij='/home/augustin/glassfish4/javadb/bin/./ij';
+appclient='/home/augustin/glassfish4/glassfish/bin/./appclient';
 DeleteBDD(){
 $ij deleteTable.sql;
 }
 CreateAdminAccess(){
 $ij createAdminUser.sql;
 }
-$asadmin --6969 stop-database
-$asadmin --6969 stop-domain
+$asadmin --port 6969 stop-database
+$asadmin --port 6969 stop-domain
 echo " ########################################"
 echo " ####### Restarting glassfish ... #######"
 echo " ########################################"
@@ -19,3 +20,4 @@ DeleteBDD
 $asadmin deploy --port 6969 --name ecomear --contextroot "ecom" target/ecomear-0.1.0.ear
 $asadmin get-client-stubs --port 6969 --appname ecomear target/
 CreateAdminAccess
+$appclient -jar target/ecomearClient.jar
